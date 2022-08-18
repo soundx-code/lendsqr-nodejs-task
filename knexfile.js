d96e89e50a1d3fb6c28a14b5b567af5b21a79197
soundx-code <soundxcode@gmail.com>
2022-08-18 16:19:20 +0100

    made some changes

--- knexfile.js
+++ knexfile.js
@@ -23,6 +23,24 @@ module.exports = {
     }
   },
 
+  test: {
+    client: 'mysql',
+    connection: {
+      host: process.env.TEST_HOST,
+      database: process.env.TEST_DB_NAME,
+      user:     process.env.TEST_DB_USER,
+      password: process.env.TEST_DB_PASSWORD,
+      port: process.env.TEST_DB_PORT
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
   production: {
     client: 'mysql',
     connection: {
