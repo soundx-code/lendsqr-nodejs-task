b51f295e3263682498c05a01b7c4df89a97af72f
soundx-code <soundxcode@gmail.com>
2022-08-18 18:54:09 +0100

    made some changes to the users unit test

--- server.js
+++ server.js
@@ -1,26 +1,30 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require("express");
+const bodyParser = require("body-parser");
 const database = require("./config/database");
-const cors = require('cors');
-const routes = require('./routes')
-const app = express()
+const cors = require("cors");
+const routes = require("./routes");
+const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use(cors());
 app.options("*", cors());
 
 app.use(routes);
 
+if (process.env.NODE_ENV !== "test") {
+  database
+    .raw("select 1+1 as result")
+    .then(() => {
+      console.log("Connection to database has been established successfully.");
 
-database.raw('select 1+1 as result')
-.then(() => {
-    console.log('Connection to database has been established successfully.');
+      app.listen(process.env.PORT || 4000, () => console.log("Listening...."));
+    })
+    .catch((err) => {
+      console.log(err);
+      process.exit(1);
+    });
+}
 
-    app.listen(process.env.PORT || 4000, () => console.log('Listening....'))
-})
-.catch(err => { 
-    console.log(err);
-    process.exit(1);
-  });
+module.exports = app;
--- tests/user.test.js
+++ tests/user.test.js
@@ -1,3 +1,71 @@
-test("Testing", () => {
-    
-})
+const request = require("supertest");
+const app = require("../server");
+
+let token;
+beforeAll(async () => {
+  await request(app).post("/api/register").send({
+    first_name: "Thomas",
+    last_name: "Balogun",
+    phone_number: "07051953939",
+    password: "123456",
+  });
+
+  const response = await request(app).post("/api/login").send({
+    phone_number: "07051953939",
+    password: "123456",
+  });
+  token = response.body.token;
+});
+
+describe("User", () => {
+  it("should register a user without error message", async () => {
+    const response = await request(app).post("/api/register").send({
+      first_name: "Amanda",
+      last_name: "Yakubu",
+      phone_number: "09081882902",
+      password: "123456",
+    });
+    expect(response.statusCode).toEqual(200);
+    expect(response.body.message).toEqual("Registration successful!");
+  });
+
+  it("should return validation error if required input are not filled when registering", async () => {
+    const response = await request(app).post("/api/register").send({
+      first_name: "",
+      last_name: "",
+      phone_number: "",
+      password: "",
+    });
+
+    expect(response.statusCode).toEqual(400);
+    expect(response.body).toHaveProperty('errors')
+  });
+
+  it("should login a user without error message", async () => {
+    const response = await request(app).post("/api/login").send({
+      phone_number: "09038533232",
+      password: "123456",
+    });
+    expect(response.statusCode).toEqual(200);
+    expect(response.body).toHaveProperty('token');
+    expect(response.body.message).toEqual("Logged in successfully!");
+  });
+
+  it("should return validation error if required input are not filled during login", async () => {
+    const response = await request(app).post("/api/login").send({
+      phone_number: "",
+      password: "",
+    });
+    expect(response.statusCode).toEqual(400);
+    expect(response.body).toHaveProperty('errors')
+  });
+
+  it("should return error for invalid login details", async () => {
+    const response = await request(app).post("/api/login").send({
+      phone_number: "08100300203",
+      password: "123051",
+    });
+    expect(response.statusCode).toEqual(401);
+    expect(response.body.message).toEqual("Invalid phone_number or password");
+  });
+});
